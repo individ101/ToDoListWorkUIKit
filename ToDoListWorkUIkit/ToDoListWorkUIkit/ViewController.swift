d963af2853bf943af37a1c2f4eaa91f6f778c4a3
individ101 <individ101@gmail.com>
2025-07-18 20:35:48 +0300

    init first files

---
+++ ToDoListWorkUIkit/ToDoListWorkUIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListWorkUIkit
+//
+//  Created by Abubakar on 18.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
