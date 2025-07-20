83f8d808fc49fa57718c4d4449b9397062028780
individ101 <individ101@gmail.com>
2025-07-20 15:44:24 +0300

    create NetworkManager to jsonApi

--- ToDoListWorkUIkit/ToDoListWorkUIkit/NetworkManager/NetworkManager.swift
+++ ToDoListWorkUIkit/ToDoListWorkUIkit/NetworkManager/NetworkManager.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchTodo(completion: @escaping(Result<TodoResponse, Error>) -> ()) {
+        let url = URL(string: "https://dummyjson.com/todos")!
+        
+        URLSession.shared.dataTask(with: url) { data, response, err in
+            if let error = err {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
+                }
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let todoResponse = try decoder.decode(TodoResponse.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(todoResponse))
+                }
+            }
+            catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }.resume()
+    }
+}
--- ToDoListWorkUIkit/ToDoListWorkUIkit/Screens/Model/TodoModel.swift
+++ ToDoListWorkUIkit/ToDoListWorkUIkit/Screens/Model/TodoModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct TodoItem: Codable {
+    let id: Int
+    let todo: String
+    let completed: Bool
+    let userId: Int
+}
+
+struct TodoResponse: Codable {
+    let todos: [TodoItem]
+    let total: Int
+    let skip: Int
+    let limit: Int
+}
