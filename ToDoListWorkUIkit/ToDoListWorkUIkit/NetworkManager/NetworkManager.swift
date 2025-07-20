7f600ebf8b38d5199e043625a56ee05e383cf614
individ101 <individ101@gmail.com>
2025-07-20 10:51:43 +0300

    refact

---
+++ ToDoListWorkUIkit/ToDoListWorkUIkit/NetworkManager/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  ToDoListWorkUIkit
+//
+//  Created by Abubakar on 20.07.2025.
+//
+
+import Foundation
---
+++ ToDoListWorkUIkit/ToDoListWorkUIkit/Screens/Model/TodoModel.swift
@@ -0,0 +1,8 @@
+//
+//  TodoModel.swift
+//  ToDoListWorkUIkit
+//
+//  Created by Abubakar on 20.07.2025.
+//
+
+import Foundation
